fcbc8aa69c3034ab476aaa5fa890dfb22f0a55e6
Kuba Biel <jakubiel@o2.pl>
2022-02-03 23:35:30 +0100

    s

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello!!!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send(`
+  <div>
+    <form method="post">
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <input name="passwordconfirmation" placeholder="confirm password" />
+      <button>Sign Up</button>
+    </form>
+  </div>
+  `);
+});
+
+app.listen(3000, () => {
+	console.log("listening");
+});
+
+app.post("/", (req, res) => {
+	res.send("account created");
+});
